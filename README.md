cd9466674f508fe557b78e25d2b7c275de57db7c
ushuz <hszqqq12@gmail.com>
2012-09-27 13:03:38 +0800

    update readme

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Autolecture, for BJFU
 TODO
 ----
 目前项目以基本完成，debug语句仍保留在源文件中，生成outlook appointment的call被注释。
-*   修改appointment的recurrence类型为每周发生，使一门课程成为一个系列约会
-*   修改公用常量的提供方式
+*   <s>修改appointment的recurrence类型为每周发生，使一门课程成为一个系列约会</s>
+*   <s>修改公用常量的提供方式</s>
 *   完善docstring及注释
 *   完善代码风格
--- autolecture.py
+++ autolecture.py
@@ -6,8 +6,8 @@ import logging
 import win32com.client
 
 
-BUILDINGS = (u'',u'一教',u'二教',u'三教',u'四教',u'体育场')                                        #授课教学楼
-START_TIMES = {'12':'8:00','34':'10:10','56':'13:30','78':'15:40','910':'19:00'}                        #课程开始时间
+BUILDINGS = {"1": u'一教', "2": u'二教', "3": u'三教', "4": u'四教'}                                  #授课教学楼
+START_TIMES = {'1':'8:00', '2':'10:10', '3':'13:30', '4':'15:40', '5':'19:00'}                        #课程开始时间
 DATES = {}
 
 
@@ -20,14 +20,17 @@ class Class:
         self.dataDict = {}
         self._parse()
         self._getExtraWeeks()
-        self.save()
+        # self.save()
     
     def _parse(self):
         u'''Class._parse私有方法
     从已拆分好的rawList中解析出location、subject、duration、startTime、weekDayNo等属性
     并储存到dataDict'''
         self.dataDict['subject'] = self.rawList[1]
-        self.dataDict['location'] = BUILDINGS[int(self.rawList[2][0])] + self.rawList[2][1:]
+        if self.rawList[3][0] in BUILDINGS.keys():
+            self.dataDict['location'] = BUILDINGS[self.rawList[3][0]] + self.rawList[3][1:]
+        else:
+            self.dataDict["location"] = self.rawList[3]
         self.dataDict['weekDayNo'] = self.rawList[0][0]
         if u'二教' in self.dataDict['location'] and self.rawList[0][1:] == '34':
             self.dataDict['duration'] = 100
@@ -39,27 +42,27 @@ class Class:
     def _getExtraWeeks(self):
         u'''Class._getExtraWeeks私有方法
     获取无课周次(extraWeeks)、系列开始日期(startDate)和结束日期(endDate)'''
-        tmp = self.rawList[3].split(',')
+        tmp = self.rawList[2].split(',')
 
         #将 'A-B' 形式的recurrence weeks展开
         for i in tmp:
             if '-' in i:
                 t = i.split('-')
-                tmp += [str(j) for j in range(int(t[0]), int(t[1])+1)]         #使用list comprehension将list中每个元素都转变为str
-                tmp[tmp.index(i)] = None                                  #展开完成后清空原位置元素
+                tmp += [str(j) for j in xrange(int(t[0]), int(t[1])+1)]                 #使用list comprehension将list中每个元素都转变为str
+                tmp[tmp.index(i)] = None                                                #展开完成后清空原位置元素
 
-        tmp = [int(i) for i in tmp if i is not None]                        #将tmp里的元素都转换为int并去掉值为None的元素以排序
+        tmp = [int(i) for i in tmp if i is not None]                                    #将tmp里的元素都转换为int并去掉值为None的元素以排序
         tmp.sort()
 
-        allWeeks = [str(i) for i in range(tmp[0], tmp[-1]+1)]               #allWeeks的元素均为str方便后续操作
+        allWeeks = [str(i) for i in xrange(tmp[0], tmp[-1]+1)]                          #allWeeks的元素均为str方便后续操作
 
-        self.dataDict['extraWeeks'] = [i for i in allWeeks if int(i) not in tmp]          #无课的周次
-        self.recurrWeeks = [str(i) for i in tmp]                                        #上课的周次
+        self.dataDict['extraWeeks'] = [i for i in allWeeks if int(i) not in tmp]        #无课的周次
         
-        self.dataDict['startDate'] = DATES[int(allWeeks[0] + self.rawList[0][0])]      #第一次上课日期    即recurrencePatternStartDate
-        self.dataDict['endDate'] = DATES[int(allWeeks[-1] + self.rawList[0][0])]       #最后一次上课日期  即recurrencePatternEndDate
+        self.dataDict['startDate'] = DATES[int(allWeeks[0] + self.rawList[0][0])]       #第一次上课日期    即recurrencePatternStartDate
+        self.dataDict['endDate'] = DATES[int(allWeeks[-1] + self.rawList[0][0])]        #最后一次上课日期  即recurrencePatternEndDate
 
-        log(self.dataDict)
+        for k,v in self.dataDict.items():
+            log(u"%s: %s" % (k, v))
 
     def save(self):
         u'''Class.save方法
@@ -68,7 +71,7 @@ class Class:
             apptGen(self.dataDict)
             print u"创建成功: %s" % self.dataDict["subject"]
         except Exception, e:
-            log(e)
+            logging.error(e)
             print u"创建失败: %s" % self.dataDict["subject"]
             return
 
@@ -79,19 +82,20 @@ def apptGen(dataDict):
     o = win32com.client.Dispatch("Outlook.Application")     #新建一个Outlook.Application实例 o
     a = o.CreateItem(1)                                     #新建一个AppointmentItem实例 a
 
-    a.Start = dataDict['startDate']+' '+dataDict['startTime']    #设置课程开始时间
-    a.Duration = dataDict['duration']                       #设置课程时长
-    a.Subject = dataDict['subject']                         #设置课程名称
-    a.Location = dataDict['location']                       #设置授课教室
+    #补完实例 a 的各种属性
+    a.Start = dataDict['startDate']+' '+dataDict['startTime']
+    a.Duration = dataDict['duration']
+    a.Subject = dataDict['subject']
+    a.Location = dataDict['location']
     a.ReminderSet = False
 
     #创建约会系列
     #每周一次，从该课程第一周到最后一周
     p = a.GetRecurrencePattern()                            #获取AppointmentItem.GetRecurrencePattern()对象以修改a的重复类型
     p.RecurrenceType = 1                                    #设置重复类型为 1 == olRecursWeekly
-    p.PatternStartDate = dataDict['startDate']              #设置重复开始日期
-    p.PatternEndDate = dataDict['endDate']                  #设置重复结束日期
-    a.Save()                                                #创建
+    p.PatternStartDate = dataDict['startDate']
+    p.PatternEndDate = dataDict['endDate']
+    a.Save()
     log(u"已创建: %s %s\t多余课程: %d节" % (a.Start, a.Subject, len(dataDict["extraWeeks"])))
     
     #删除系列中的无效约会
@@ -128,9 +132,9 @@ def apptGen(dataDict):
     # elif s == 1:
         # print u'请输入学期总周数(int.dd)'
     # elif s == 2:
-        # print u'''请输入课程信息(str.'WSS SUBJECT LOCATION RECURRWEEKS')
-# eg. '112 高等数学 1108 2-11,14,17'"
-    # 周一1-2节 高等数学 一教108 2-11,14,17周上课'''
+        # print u'''请输入课程信息(str.'WSS SUBJECT RECURRWEEKS LOCATION')
+# eg. '11 高等数学 1108 2-11,14,17'"
+    # 周一1-2节 高等数学 2-11,14,17周 一教108上课'''
     # elif s == 'debug':
         # print '###FORDEBUG###'
 
@@ -141,40 +145,42 @@ def genDateDict(termFirstDayStr,termTotalWeeks):
 
     weekDayList = []
     weekNo = 11             #表示第一周周一
-    global DATES
 
     #生成工作日(weekday)日期列表(list)
-    for i in range(0,termTotalWeeks*7):
+    for i in xrange(0,termTotalWeeks*7):
         if not((i-5)%7 == 0 or (i-6)%7 == 0):
             weekDayList += [str(termFirstDay + datetime.timedelta(i))]
 
     #生成工作日(weekday)日期词典(dict)，以(AB(A:周目;B:日目))为键(key)
-    for i in range(1,termTotalWeeks*5+1):
+    global DATES
+    for i in xrange(1,termTotalWeeks*5+1):
         DATES[weekNo] = weekDayList[i-1]
         if i % 5 == 0:
             weekNo += 6
         else:
             weekNo += 1
 
-def Pass(*argv):
-    pass
-
-def main():
-    genDateDict(termFirstDayStr="20120903", termTotalWeeks=20)                   #调用genDateDict生成DATES
-
-    Class(u"234 大学英语 1314 1-4,6-14")
-    Class(u"112 管理学基础 2506 1-4,6,8-14,16-17")
+def main(termFirstDayStr, termTotalWeeks, lectures):
+    #生成DATES常量
     
+    genDateDict(termFirstDayStr, termTotalWeeks)
+    
+    for i in lectures:
+        Class(i.decode("utf-8")).save()
 
 if __name__ == '__main__':
-    debug=False
+    
+    debug = True
     
     if debug:
         logging.basicConfig(level=logging.DEBUG,
                     format='%(levelname)s - %(message)s',
                     )
-        log = logging.debug
     else:
-        log = Pass
-
-    main()
+        logging.basicConfig(level=logging.ERROR,
+                    format='%(levelname)s - %(message)s',
+                    )
+    log = logging.debug
+    
+    
+    main("20120903", 20, [])
